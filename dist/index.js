32cde228a49aa87af04cdac1b6444fdee9d1b5e2
Matan Gubkin <Matangub@gmail.com>
2026-06-20 09:48:13 +0300

    Refactor project structure and add Ultracite for code quality enforcement

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,10 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-
 const app = new Hono();
-
 app.get("/", (c) => c.text("Hello Hono!"));
-
-serve(
-  {
+serve({
     fetch: app.fetch,
     port: 3000,
-  },
-  (info) => {
+}, (info) => {
     console.log(`Server is running on http://localhost:${info.port}`);
-  }
-);
+});
